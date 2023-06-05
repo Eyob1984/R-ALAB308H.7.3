d1f41febdea17d6e155567d24c708c457259090d
Eyob Tamir <51892714+Eyob1984@users.noreply.github.com>
2023-06-05 08:57:00 -0700

    created Person class

--- lab.js
+++ lab.js
@@ -19,3 +19,58 @@ class Hamster {
     return this.price;
   }
 }
+
+
+// Person Class
+
+class Person {
+  constructor(name, age = 0, height = 0, weight = 0, mood = 0, hamsters = [], bankAccount = 0){
+    this.name = name;
+    this.age = age;
+    this.height = height;
+    this.weight = weight;
+    this.mood = mood;
+    this.hamsters = hamster;
+    this.bankAccount = bankAccount;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getAge() {
+    return this.age;
+  }
+
+  getWeight() {
+    return this.weight
+  }
+
+  greet() {
+    console.log(`My name is ${this.name}.`)
+  }
+
+  eat() {
+    this.weight++;
+    this.mood++;
+  }
+
+  exercise() {
+    this.weight--;
+  }
+
+  ageUp() {
+    this.age++;
+    this.height++;
+    this.weight++;
+    this.mood--;
+    this.bankAccount + 10;
+  }
+
+  buyHamster(hamster) {
+    this.hamsters.push(hamster);
+    this.mood + 10;
+    this.bankAccount - hamster; 
+
+  }
+}
