0f66ab29fec5935a3a7c349ccbaab485469520a9
Eyob Tamir <51892714+Eyob1984@users.noreply.github.com>
2023-06-05 08:37:03 -0700

    created Hamster class

---
+++ lab.js
@@ -0,0 +1,21 @@
+// Hamster Class
+
+class Hamster {
+  constructor(owner = '', name, price){
+    this.owner = owner;
+    this.name = name;
+    this.price = price;
+  }
+
+  wheelRun() {
+    console.log('squeak squeak')
+  }
+
+  eatFood() {
+    console.log('nibble nibble')
+  }
+
+  getPrice() {
+    return this.price;
+  }
+}
